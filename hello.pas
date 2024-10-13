e38e61677f47b346bd7952cf972fbcd788e4eaf6
Роман Белый <roma13beliy@gmail.com>
2024-10-13 17:42:50 +0300

    сделал задание

--- hello.pas
+++ hello.pas
@@ -1,3 +1,18 @@
 ﻿begin
-  print('Hello world!');
+  var l:=readstring('Введите ваш язык:');
+  if l='Английский' then
+    println('Hello!');
+  if l='Русский' then
+    println('Привет!');
+  if l='Испанский' then
+    println('Hola!');
 end.
+
+{Введите ваш язык: Английский
+Hello!
+
+Введите ваш язык: Русский
+Привет!
+
+Введите ваш язык: Испанский
+Hola!}
